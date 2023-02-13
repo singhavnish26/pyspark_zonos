32f675e452130d95a73d7877f31b250fed0489fc
singhavnish26 <singhavnishsingh@gmail.com>
2023-02-14 01:38:14 +0530

    not working very well, need to look it again and discover logic

--- device_measurement_to_df.py
+++ device_measurement_to_df.py
@@ -1,62 +1,68 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import explode, struct
+from pyspark.sql.functions import from_json, col
+from pyspark.sql.types import StructType, StructField, StringType, ArrayType, DoubleType, TimestampType
 
-# Create a Spark session
-spark = SparkSession.builder.appName("DevMeasurementToDF").getOrCreate()
-spark.sparkContext.setLogLevel('ERROR')
+spark = SparkSession.builder.appName("KafkaDataFrame").getOrCreate()
+sc = spark.sparkContext
+sc.setLogLevel('ERROR')
 
-# Read data from the Kafka topic as a stream
-df = spark \
-  .readStream \
-  .format("kafka") \
-  .option("kafka.bootstrap.servers", "zonos.engrid.in:9092") \
-  .option("subscribe", "ext_device-measurement_10121") \
-  .option("startingOffsets", "earliest") \
-  .load()
+kafka_topic = "ext_device-measurement_10121"
 
-# Define the schema for the data
-from pyspark.sql.types import StructType, StructField, StringType, ArrayType, TimestampType, DoubleType
-
-schema = StructType([
-  StructField("device", StringType()),
-  StructField("measureTime", TimestampType()),
-  StructField("measureTime", TimestampType()),
-  StructField("receiveTime", TimestampType()),
-  StructField("persistTime", TimestampType()),
-  StructField("readingReason", StringType()),
-  StructField("dataPoints", ArrayType(
-    StructType([
-      StructField("register", StringType()),
-      StructField("value", DoubleType()),
-      StructField("unit", StringType()),
-      StructField("measureTime", TimestampType())
-    ])
-  )),
-  StructField("status", ArrayType(StringType())),
-  StructField("profile", StringType())
+# Define the schema for the JSON data
+json_schema = StructType([
+    StructField("device", StringType(), True),
+    StructField("measureTime", TimestampType(), True),
+    StructField("receiveTime", TimestampType(), True),
+    StructField("persistTime", TimestampType(), True),
+    StructField("readingReason", StringType(), True),
+    StructField("dataPoints", ArrayType(
+        StructType([
+            StructField("register", StringType(), True),
+            StructField("value", DoubleType(), True),
+            StructField("unit", StringType(), True),
+            StructField("measureTime", TimestampType(), True)
+        ])
+    ), True),
+    StructField("status", ArrayType(StringType()), True),
+    StructField("profile", StringType(), True)
 ])
 
-# Flatten the nested JSON data
-parsed_df = df.select(
-  df.value.cast("string").alias("json_data"),
-  df.timestamp.alias("event_timestamp")
+# Read the Kafka topic into a DataFrame
+df = spark \
+    .readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "zonos.engrid.in:9092") \
+    .option("subscribe", kafka_topic) \
+    .option("startingOffsets", "earliest") \
+    .load() \
+    .selectExpr("CAST(value AS STRING) as json_data")
+df = df.filter(col("value").isNotNull())
+# Parse the JSON data
+parsed_df = df.select(from_json(col("json_data"), json_schema).alias("data"))
+flattened_df = parsed_df.selectExpr(
+    "data.device",
+    "data.measureTime",
+    "data.receiveTime",
+    "data.persistTime",
+    "data.readingReason",
+    "explode(data.dataPoints) as dataPoints"
+)
+flattened_df = flattened_df.selectExpr(
+    "device",
+    "measureTime",
+    "receiveTime",
+    "persistTime",
+    "readingReason",
+    "dataPoints.register",
+    "dataPoints.value",
+    "dataPoints.unit",
+    "dataPoints.measureTime as dataPointMeasureTime"
 )
 
-flattened_df = parsed_df.select(
-  "event_timestamp",
-  struct(parsed_df.json_data).alias("data")
-).select(
-  "event_timestamp",
-  "data.device",
-  "data.measureTime",
-  "data.receiveTime",
-  "data.persistTime",
-  "data.readingReason",
-  explode("data.dataPoints").alias("dataPoints"),
-  "data.status",
-  "data.profile"
-)
+# Print the parsed DataFrame
+query = flattened_df.writeStream \
+    .outputMode("append") \
+    .format("console") \
+    .start()
 
-# Print the flattened dataframe as a table
-query = flattened_df.writeStream.format("console").option("truncate", "false").start()
 query.awaitTermination()
