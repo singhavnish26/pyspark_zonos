78ffe049ce81b5f0bfb7469cff65c2d132699a10
singhavnish26 <singhavnishsingh@gmail.com>
2023-03-13 17:15:53 +0530

    commit before radical change, moving kafka_reads to function

--- stream_except/stream.py
+++ stream_except/stream.py
@@ -3,28 +3,31 @@ from configparser import ConfigParser
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, TimestampType, ArrayType, FloatType, DateType, DoubleType
 from pyspark.sql.functions import from_json, col, to_date, current_date
 from pyspark.sql import SparkSession
-from schema import schema1, schema2
+from schema import schema1, schema2, schema3, schema4, schema5, schema6, schema7, schema8
 import logging
-
 logger = logging.getLogger(__name__)
+# Configure logging
+logging.basicConfig(filename='app.log', level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 # Read configuration file
 config = ConfigParser()
 config.read('config.ini')
 
-# Define the Kafka topic name and host
+# Define the Kafka topic name and host from config file
 kafka_host = config.get('kafka', 'kafka_host')
 topic1 = config.get('kafka', 'event_topic')
 topic2 = config.get('kafka', 'telemetry_topic')
-
-#Get Cassandra Host and Credentials
+topic3 = config.get('kafka', 'device_topic')
+topic4 = config.get('kafka', 'measurement_topic')
+topic5 = config.get('kafka', 'regstat_topic')
+topic6 = config.get('kafka', 'process_topic')
+topic7 = config.get('kafka', 'parameter_topic')
+topic8 = config.get('kafka', 'proffact_topic')
+#Get Cassandra Host and Credentials from config file
 cassandra_host = config.get('cassandra', 'cass_host')
 cassandra_user = config.get('cassandra', 'username')
 cassandra_password = config.get('cassandra', 'password')
-
-# Configure logging
-logging.basicConfig(filename='app.log', level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Create Spark session
 spark = SparkSession \
@@ -57,6 +60,62 @@ df2 = spark \
     .option("subscribe", topic2) \
     .option("startingOffsets", "earliest") \
     .load()
+
+df3 = spark \
+    .readStream \
+    .format ("kafka") \
+    .option ("kafka.bootstrap.servers", kafka_host) \
+    .option("subscribe", topic3) \
+    .option("startingOffsets", "earliest") \
+    .load() \
+    .selectExpr("CAST(value AS STRING)")
+
+df4 = spark \
+    .readStream \
+    .format ("kafka") \
+    .option ("kafka.bootstrap.servers", kafka_host) \
+    .option("subscribe", topic4) \
+    .option("startingOffsets", "earliest") \
+    .load() \
+    .select(from_json(col("value").cast("string"), schema4).alias("data")) \
+    .select("data.*")
+
+df5 = spark \
+    .readStream \
+    .format ("kafka") \
+    .option ("kafka.bootstrap.servers", kafka_host) \
+    .option("subscribe", topic5) \
+    .option("startingOffsets", "earliest") \
+    .load() \
+    .select(from_json(col("value").cast("string"), schema5).alias("data")) \
+
+df6 = spark \
+    .readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", kafka_host) \
+    .option("startingOffsets", "earliest") \
+    .option("subscribe", topic6).load() \
+    .selectExpr("CAST(value AS STRING)") \
+    .select(from_json(col("value"), schema6).alias("parsed_value"))
+
+df7 = spark \
+    .readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", kafka_host) \
+    .option("subscribe", topic7) \
+    .option("startingOffsets", "earliest") \
+    .load()
+
+df8 = spark \
+    .readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", kafka_host) \
+    .option("subscribe", topic8) \
+    .option("startingOffsets", "earliest") \
+    .load() \
+    .selectExpr("CAST(value AS STRING)") \
+    .select(from_json(col("value"), schema8).alias("data")) \
+    .select("data.*") 
 
 # Process event data
 event_df = df1.selectExpr("CAST(value AS STRING)") \
@@ -97,9 +156,6 @@ query2 = telemetry_df.writeStream \
     .option("checkpointLocation", telemetry_query_checkpoint) \
     .foreachBatch(lambda df, epochId: write_to_cassandra(df, epochId, "telemetry"))
 
-
-"""telemetry_df.printSchema()
-event_df.printSchema()"""
 
 query1.start()
 query2.start()
