d6e3aaa5f100d964f71fc8207f735849231c7d32
singhavnish26 <singhavnishsingh@gmail.com>
2023-03-13 16:48:11 +0530

    Exception and config file added

--- stream_except/stream.py
+++ stream_except/stream.py
@@ -3,11 +3,25 @@ from configparser import ConfigParser
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, TimestampType, ArrayType, FloatType, DateType, DoubleType
 from pyspark.sql.functions import from_json, col, to_date, current_date
 from pyspark.sql import SparkSession
-from schemas import schema1, schema2
+from schema import schema1, schema2
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 # Read configuration file
 config = ConfigParser()
 config.read('config.ini')
+
+# Define the Kafka topic name and host
+kafka_host = config.get('kafka', 'kafka_host')
+topic1 = config.get('kafka', 'event_topic')
+topic2 = config.get('kafka', 'telemetry_topic')
+
+#Get Cassandra Host and Credentials
+cassandra_host = config.get('cassandra', 'cass_host')
+cassandra_user = config.get('cassandra', 'username')
+cassandra_password = config.get('cassandra', 'password')
 
 # Configure logging
 logging.basicConfig(filename='app.log', level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -16,19 +30,16 @@ logging.basicConfig(filename='app.log', level=logging.ERROR, format='%(asctime)s
 spark = SparkSession \
     .builder \
     .appName("KafkaMultiStreamToCassandra") \
-    .config("spark.cassandra.connection.host", "13.232.25.194") \
-    .config("spark.cassandra.auth.username", "cassandra") \
-    .config("spark.cassandra.auth.password", "cassandra") \
+    .config("spark.cassandra.connection.host", cassandra_host) \
+    .config("spark.cassandra.auth.username", cassandra_user) \
+    .config("spark.cassandra.auth.password", cassandra_password) \
     .getOrCreate()
 
 # Set the Spark log level to ERROR
 sc = spark.sparkContext
 sc.setLogLevel('ERROR')
 
-# Define the Kafka topic name and host
-kafka_host = "minor.zonos.engrid.in:9092"
-topic1 = config.get('kafka', 'event_topic')
-topic2 = config.get('kafka', 'telemetry_topic')
+
 
 # Configure kafka listeners for the relevant kafka topics
 df1 = spark \
@@ -62,35 +73,33 @@ telemetry_df = df2.selectExpr("CAST(value AS STRING)") \
     .withColumnRenamed("lastReceiveTime","lasttelemetry")
 
 #Write Data to Cassandra
+def write_to_cassandra(df, epochId, table_name):
+    try:
+        df.write \
+            .format("org.apache.spark.sql.cassandra") \
+            .mode("append") \
+            .options(table=table_name, keyspace="reporting") \
+            .save()
+    except Exception as e:
+        logger.error(f"Error occurred while processing {table_name} query: {e.__class__.__name__} - {str(e)}")
+
+event_query_checkpoint = config.get('checkpoint', 'event_query_checkpoint')
+telemetry_query_checkpoint = config.get('checkpoint', 'telemetry_query_checkpoint')
 query1 = event_df.writeStream \
     .outputMode("append") \
     .queryName("event") \
     .option("checkpointLocation", event_query_checkpoint) \
-    .foreachBatch(lambda df, epochId: 
-        try:
-            df.write \
-                .format("org.apache.spark.sql.cassandra") \
-                .mode("append") \
-                .options(table="events", keyspace="reporting") \
-                .save()
-        except Exception as e:
-            logger.error(f"Error occurred while processing query1: {e.__class__.__name__} - {str(e)}")
-    )
+    .foreachBatch(lambda df, epochId: write_to_cassandra(df, epochId, "events"))
 
 query2 = telemetry_df.writeStream \
     .outputMode("append") \
-    .queryName("event") \
+    .queryName("telemetry") \
     .option("checkpointLocation", telemetry_query_checkpoint) \
-    .foreachBatch(lambda df, epochId: 
-        try:
-            df.write \
-                .format("org.apache.spark.sql.cassandra") \
-                .mode("append") \
-                .options(table="telemetry", keyspace="reporting") \
-                .save()
-        except Exception as e:
-            logger.error(f"Error occurred while processing query1: {e.__class__.__name__} - {str(e)}")
-    )
+    .foreachBatch(lambda df, epochId: write_to_cassandra(df, epochId, "telemetry"))
+
+
+"""telemetry_df.printSchema()
+event_df.printSchema()"""
 
 query1.start()
 query2.start()
